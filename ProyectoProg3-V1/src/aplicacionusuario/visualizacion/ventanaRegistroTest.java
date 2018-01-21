e58d6d6f1744ff8f135521462fb86c0ac4bfe45b
Ander Jarauta <Ander Jarauta@DESKTOP-RDO9IDA>
2018-01-21 19:02:03 +0100

    JUnit

---
+++ ProyectoProg3-V1/src/aplicacionusuario/visualizacion/ventanaRegistroTest.java
@@ -0,0 +1,68 @@
+package aplicacionusuario.visualizacion;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+import aplicacionusuario.datos.BD;
+import aplicacionusuario.datos.Usuario;
+
+public class ventanaRegistroTest {
+	
+	
+	@Test
+	public void consultarBD(){
+		String usuario1 = "garrix";
+		String contrasenya1 = "pass";
+		String usuario2 = "anderja";
+		String contrasenya2 = "co";
+		Usuario user1 = new Usuario(usuario1, contrasenya1, "");
+		Usuario user2 = new Usuario(usuario2, contrasenya2, "");
+
+		try {
+			BD.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Usuario u1 = BD.buscarUsuario(user1);
+		Usuario u2 = BD.buscarUsuario(user2);
+
+		
+		assertEquals("Usuario esta en BD", usuario1, u1.getNombre());
+		assertEquals("Contraeña esta en BD", contrasenya1, u1.getContraseña());
+
+		assertEquals("Usuario esta en BD", usuario2, u2.getNombre());
+		assertEquals("Contraeña esta en BD", contrasenya2, u2.getContraseña());
+
+	}
+	
+	@Test
+	public void consultarContrasenyaIncorrectaBD(){
+		String usuario1 = "garrix";
+		String contrasenya1 = "pas"; // contraseña correcta "pass"
+		String usuario2 = "anderja";
+		String contrasenya2 = "CO"; // contraseña correcta "co"
+		Usuario user1 = new Usuario(usuario1, contrasenya1, "");
+		Usuario user2 = new Usuario(usuario2, contrasenya2, "");
+		
+		try {
+			BD.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Usuario u1 = BD.buscarUsuario(user1);
+		Usuario u2 = BD.buscarUsuario(user2);
+		
+		
+		
+		assertTrue("Usuario no esta en BD", u1==null);
+		assertTrue("Usuario no esta en BD", u2==null);
+
+	
+
+	}
+	
+}
